c8120993bb2ad0bd3f3c035e9655c21dcf63a916
Juan Martinez <jhonra@icloud.com>
2019-04-17 13:14:54 -0500

    updated app rb

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require 'sinatra/flash'
 require_relative "authentication.rb"
+require_relative "models.rb"
 
 #the following urls are included in authentication.rb
 # GET /login
@@ -15,6 +16,25 @@ get "/" do
 	erb :index
 end
 
+post "/videos" do
+	@video = Video.new
+	if params["title"] && params["video_url"]
+		@video.title = params["title"]
+		@video.description = params["description"]
+		@video.video_url = params["video_url"]
+		@video.user_id = current_user.id
+	end
+end
+
+get "/videos" do
+	authenticate!
+	@videos = Video.all
+	erb :videos
+end
+
+post "/posts/:id/delete" do
+	@post = Post.get(params["id"])
+end
 
 get "/dashboard" do
 	authenticate!
