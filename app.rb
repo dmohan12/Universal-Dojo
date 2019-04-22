9804d65fdabfc8d334f52352b38364c5447abfa7
Juan Martinez <jhonra@icloud.com>
2019-04-22 15:42:28 -0500

    added tag stuff (WIP)

--- app.rb
+++ app.rb
@@ -26,17 +26,50 @@ post "/videos" do
 	end
 end
 
+
+
 get "/videos" do
 	authenticate!
-	@videos = Video.all
+	@videos = Video.all(id: current_user.id)
+	#@tags = Tag.all
 	erb :videos
-end
-
-post "/posts/:id/delete" do
-	@post = Post.get(params["id"])
 end
 
 get "/dashboard" do
 	authenticate!
 	erb :dashboard
 end
+
+post "/post/create" do      #grabs backend code in creating a new post
+	authenticate!
+	vid=Video.new
+	ta = Tag.new
+
+	if params["title"] && params["description"] && params["video_url"]
+		vid.title=params["title"]
+		vid.description=params["description"]
+		vid.video_url=params["video_url"]
+		vid.save
+
+		#adding tags
+		if params["tag_name"]
+			t = params["tag_name"].split(",")
+			t.each do |tags|
+				ta.tag_name = tags
+				ta.video_id = vid.id
+				ta.save
+			end
+			
+		end
+	end 
+
+end
+
+#post "/tags" do
+#	erb :postVideo
+#end
+
+get "/post/new" do       #erb to postVideo
+	authenticate!
+	erb :postVideo
+end 
